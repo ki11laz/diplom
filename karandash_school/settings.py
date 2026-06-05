b78811115116bd313defc08f048f07da346cb3b1
ki11laz <egor.komogor@gmail.com>
2026-06-05 18:36:22 +0900

    Update settings.py

--- karandash_school/settings.py
+++ karandash_school/settings.py
@@ -127,7 +127,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://karandash.up.railway.app',
+]
 
 # Авторизация
 AUTH_USER_MODEL = 'users.CustomUser'
