75ed43055c7ad334cdb2bf1bcf8aeae934303651
ki11laz <egor.komogor@gmail.com>
2026-06-05 18:05:55 +0900

    Update urls.py

--- karandash_school/urls.py
+++ karandash_school/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include, re_path
 
 urlpatterns = [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
